b6d3d88571500d9d6977a3101fd81405789ded51
dail45 <69967573+dail45@users.noreply.github.com>
2021-10-21 20:21:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,77 +3,77 @@ import os
 import time
 import random
 
-
 app = Flask(__name__)
 RNUMS = {}
 
 
 @app.route("/")
 def hi():
-	return "Hi, world!"
+    return "Hi, world!"
+
 
 @app.route("/reg")
 def registration():
-	args = request.args
-	nums = list(range(10))
-	while True:
-		rnum = random.choices(nums, 6)
-		if rnum not in RNUMS:
-			RNUMS[rnum] = {}
-			RNUMS[rnum]["com"] = []
-			RNUMS[rnum]["ans"] = []
-			if info in args:
-				RNUMS[rnum]["info"] = info
-			break
-	return rnum
-	
-	
+    args = request.args
+    nums = list(map(str, range(10)))
+    while True:
+        rnum = str(int("".join(random.sample(nums, 4))))
+        if rnum not in RNUMS:
+            RNUMS[rnum] = {}
+            RNUMS[rnum]["com"] = []
+            RNUMS[rnum]["ans"] = []
+            if "info" in args:
+                RNUMS[rnum]["info"] = args["info"]
+            break
+    return rnum
+
+
 @app.route("/getrnums")
 def gernums():
-	return RNUMS
-	
+    return RNUMS
+
 
 @app.route("/sendcommand")
 def sendcommand():
-	args = request.args
-	rnum = args["rnum"]
-	com = args["com"]
-	RNUM[rnum]["com"].append(com)
-	return 0
-	
+    args = request.args
+    rnum = args["rnum"]
+    com = args["com"]
+    RNUMS[rnum]["com"].append(com)
+    return "0"
+
 
 @app.route("/getcommand")
 def getcommand():
-	args = request.args
-	rnum = args["rnum"]
-	while True:
-		if len(RNUM[rnum]["com"]) > 0:
-			com = RNUM[rnum]["com"].pop(0)
-			break
-		time.sleep(0.1)
-	return com
+    args = request.args
+    rnum = args["rnum"]
+    while True:
+        if len(RNUMS[rnum]["com"]) > 0:
+            com = RNUMS[rnum]["com"].pop(0)
+            break
+        time.sleep(0.1)
+    return com
 
 
 @app.route("/getanswer")
 def getanswer():
-	args = request.args
-	rnum = args["rnum"]
-	while True:
-		if len(RNUM[rnum]["ans"]) > 0:
-			ans = RNUM[rnum]["ans"].pop(0)
-			break
-		time.sleep(0.1)
-	return ans
+    args = request.args
+    rnum = args["rnum"]
+    while True:
+        if len(RNUMS[rnum]["ans"]) > 0:
+            ans = RNUMS[rnum]["ans"].pop(0)
+            break
+        time.sleep(0.1)
+    return ans
 
 
 @app.route("/sendanswer")
 def sendanswer():
-	args = request.args
-	rnum = args["rnum"]
-	ans = args["ans"]
-	RNUM[rnum]["ans"].append(ans)
-	return 0
-	
+    args = request.args
+    rnum = args["rnum"]
+    ans = args["ans"]
+    RNUMS[rnum]["ans"].append(ans)
+    return "0"
+
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
